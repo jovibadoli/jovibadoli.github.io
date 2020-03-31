836042777bb38ed112413383a366921b5408c79d
jovibadoli <61387010+jovibadoli@users.noreply.github.com>
2020-03-30 21:46:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -102,8 +102,8 @@ function angle(o, v) {
 	elems[1].value = x;
 	elems[2].value = x*60;
 	elems[3].value = x*3600;
-	elems[4].value = x;
-	elems[5].value = x/2;
+	elems[4].value = x/60;
+	elems[5].value = x/90;
 	elems[6].value = x/360;
 }
 
